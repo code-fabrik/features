57709b1f5fc22499a7abc49f8f05620fef17faba
Lukas_Skywalker <git@lukasdiener.ch>
2024-08-09 13:01:11 +0000

    Install from local file instead of git

--- src/cli/install.sh
+++ src/cli/install.sh
@@ -3,5 +3,4 @@ set -e
 
 echo "Activating feature 'dokku'"
 
-git clone https://github.com/dokku/dokku.git $_CONTAINER_USER_HOME/.dokku
-mv $_CONTAINER_USER_HOME/.dokku/contrib/dokku_client.sh /usr/local/bin/dokku
+cp dokku_client.sh /usr/local/bin/dokku
