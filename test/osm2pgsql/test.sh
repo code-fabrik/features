3819f8a5ee61dc6e37d6c9655c9b13b540ba804e
Lukas_Skywalker <git@lukasdiener.ch>
2025-08-06 13:36:50 +0000

    Add osm2pgsql test

--- test/osm2pgsql/test.sh
+++ test/osm2pgsql/test.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 
-check "npx is installed" npx -v 
+check "osm2pgsql is installed" osm2pgsql --version 
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
