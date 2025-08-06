5e391a4956e0162ea501c95d42ff0e9a12e8f66d
Lukas_Skywalker <git@lukasdiener.ch>
2025-08-06 19:17:46 +0000

    Fix osm2pgsql install script

--- src/osm2pgsql/install.sh
+++ src/osm2pgsql/install.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 echo "Activating feature 'osm2pgsql'"
 
-sudo apt-get install make cmake g++ libboost-dev \
+apt-get update
+
+apt-get install -y make cmake g++ libboost-dev \
   libexpat1-dev zlib1g-dev libpotrace-dev \
   libopencv-dev libbz2-dev libpq-dev libproj-dev lua5.3 liblua5.3-dev \
-  pandoc nlohmann-json3-dev pyosmium
+  pandoc nlohmann-json3-dev pyosmium git
 
 git clone https://github.com/osm2pgsql-dev/osm2pgsql.git
 
