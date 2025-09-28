a3d74969831298f7975ab4daea4aa234f5ff8dd3
Lukas_Skywalker <git@lukasdiener.ch>
2025-09-28 18:53:44 +0200

    Fix installation command

--- src/pmtiles/install.sh
+++ src/pmtiles/install.sh
@@ -9,6 +9,6 @@ apt-get update
 
 apt-get install -y wget
 
-wget https://github.com/protomaps/go-pmtiles/releases/download/v1.28.1/go-pmtiles_1.28.1_Linux_x86_64.tar.gz pmtiles.tar.gz
+wget -O pmtiles.tar.gz https://github.com/protomaps/go-pmtiles/releases/download/v1.28.1/go-pmtiles_1.28.1_Linux_x86_64.tar.gz
 
 tar -xzf pmtiles.tar.gz -C /usr/local/bin/ && rm pmtiles.tar.gz
